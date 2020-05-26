a8f9ed1b0f35a5b0a10db4bc2a617da1bfdf2ea6
aleksandar9999a <aleksandar9999a@gmail.com>
2020-05-26 08:46:15 +0300

    Added auth middleware

--- api/pages.js
+++ api/pages.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const models = require('./../models/');
+const { auth } = require('./../modules');
 
 router.get('/about', (req, res, next) => {
     models.AboutMe.find().then(info => {
@@ -9,16 +10,6 @@ router.get('/about', (req, res, next) => {
         }
         res.send(404);
     }).catch(next);
-});
-
-router.post('/about', (req, res, next) => {
-    const { description, courses } = req.body;
-    models.AboutMe.create({ description, courses }).then(info => res.send(info)).catch(next);
-})
-
-router.put('/about', (req, res, next) => {
-    const { id, description, courses } = req.body;
-    models.AboutMe.updateOne({ _id: id }, { description, courses }).then(info => res.send(info)).catch(next);
 });
 
 router.get('/skills', (req, res, next) => {
@@ -31,26 +22,36 @@ router.get('/skills', (req, res, next) => {
     }).catch(next);
 });
 
-router.post('/Skills', (req, res, next) => {
-    const { description, experience } = req.body;
-    models.Skills.create({ description, experience }).then(info => res.send(info)).catch(next);
-})
-
-router.put('/Skills', (req, res, next) => {
-    const { id, description, experience } = req.body;
-    models.Skills.updateOne({ _id: id }, { description, experience }).then(info => res.send(info)).catch(next);
-});
-
 router.get('/projects', (req, res, next) => {
     models.Projects.find().then(projects => res.send(projects)).catch(next);
 });
 
-router.post('/projects', (req, res, next) => {
+router.post('/about', auth, (req, res, next) => {
+    const { description, courses } = req.body;
+    models.AboutMe.create({ description, courses }).then(info => res.send(info)).catch(next);
+})
+
+router.post('/Skills', auth, (req, res, next) => {
+    const { description, experience } = req.body;
+    models.Skills.create({ description, experience }).then(info => res.send(info)).catch(next);
+})
+
+router.post('/projects', auth, (req, res, next) => {
     const { title, description, images } = req.body;
     models.Projects.create({ title, description, images }).then(project => res.send(project)).catch(next);
 })
 
-router.put('/projects', (req, res, next) => {
+router.put('/about', auth, (req, res, next) => {
+    const { id, description, courses } = req.body;
+    models.AboutMe.updateOne({ _id: id }, { description, courses }).then(info => res.send(info)).catch(next);
+});
+
+router.put('/Skills', auth, (req, res, next) => {
+    const { id, description, experience } = req.body;
+    models.Skills.updateOne({ _id: id }, { description, experience }).then(info => res.send(info)).catch(next);
+});
+
+router.put('/projects', auth, (req, res, next) => {
     const { id, title, description, images } = req.body;
     models.Projects.updateOne({ _id: id }, { title, description, images }).then(project => res.send(project)).catch(next);
 });
---
+++ modules/auth.js
@@ -0,0 +1,18 @@
+const jwt = require('./jwt');
+const models = require('../models');
+
+module.exports = function (req, res, next) {
+    const token = req.cookies['auth_cookie'];
+    jwt.verify(token)
+        .then(({ id }) => models.User.findById(id))
+        .then(user => {
+            if (!user) {
+                return Promise.reject();
+            }
+            req.user = user;
+            next();
+        })
+        .catch(() => {
+            res.status(401).send('[UNAUTHORIZED]');
+        });
+}
