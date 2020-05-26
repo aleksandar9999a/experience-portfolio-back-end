2ab5d6431c1afa572e130c0d6bcd95dae7c60859
aleksandar9999a <aleksandar9999a@gmail.com>
2020-05-26 06:28:48 +0300

    Added secret

--- environments/environments.js
+++ environments/environments.js
@@ -2,7 +2,8 @@ const env = process.env.NODE_ENV || 'development';
 const config = {
     development: {
         port: process.env.PORT || 4000,
-        dbURL: 'mongodb://localhost:27017/experience-portfolio'
+        dbURL: 'mongodb://localhost:27017/experience-portfolio',
+        secret: 'qkoedasiumen'
     },
     production: {}
 };
