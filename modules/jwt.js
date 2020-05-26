3e43969089310402f8b894ba23be99a154fa2100
aleksandar9999a <aleksandar9999a@gmail.com>
2020-05-26 07:38:59 +0300

    Added module with create, verify for json tokens

---
+++ modules/jwt.js
@@ -0,0 +1,26 @@
+const config = require('./environments/environments');
+const jwt = require('jsonwebtoken');
+
+module.exports = {
+    create: function (data) {
+        return new Promise((resolve, reject) =>
+            jwt.sign(data, config.secret, { expiresIn: '2 days' }, (err, token) => {
+                if (err) {
+                    reject(err);
+                    return;
+                }
+                resolve(token);
+            }));
+    },
+    verify: function (token) {
+        return new Promise((resolve, reject) => {
+            jwt.verify(token, config.secret, (err, data) => {
+                if (err) {
+                    reject(err);
+                    return;
+                }
+                resolve(data);
+            });
+        });
+    }
+};
