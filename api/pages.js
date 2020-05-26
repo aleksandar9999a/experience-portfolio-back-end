7f9808113a10e8d699a36c858a88768654e207df
aleksandar9999a <aleksandar9999a@gmail.com>
2020-05-26 07:09:23 +0300

    Added get, post and put for about

--- api/pages.js
+++ api/pages.js
@@ -1,7 +1,24 @@
 const router = require('express').Router();
+const models = require('./../models/');
 
-router.get('/', (req, res, next) => {
-    res.send('qko')
+router.get('/about', (req, res, next) => {
+    models.AboutMe.find().then(info => {
+        if (!!info[0]) {
+            res.send(info[0]);
+            return;
+        }
+        res.send(404);
+    }).catch(next);
+});
+
+router.post('/about', (req, res, next) => {
+    const { description, courses } = req.body;
+    models.AboutMe.create({ description, courses }).then(info => res.send(info)).catch(next);
+})
+
+router.put('/about', (req, res, next) => {
+    const { id, description, courses } = req.body;
+    models.User.updateOne({ _id: id }, { description, courses }).then(info => res.send(info)).catch(next);
 });
 
 module.exports = router;
