9993516b60408af27faa7006400e73b6f642baee
aleksandar9999a <aleksandar9999a@gmail.com>
2020-05-26 11:02:27 +0300

    Added route for update, fix sendStatus, added devType

--- api/auth.js
+++ api/auth.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const models = require('./../models/');
 const modules = require('./../modules');
+const { auth } = require('./../modules');
 
 router.get('/', (req, res, next) => {
     models.User.find().then(users => {
@@ -8,13 +9,13 @@ router.get('/', (req, res, next) => {
             res.send(users[0]);
             return;
         }
-        res.send(404);
+        res.sendStatus(404);
     }).catch(next);
 });
 
 router.post('/register', (req, res, next) => {
-    const { email, password, firstName, lastName } = req.body;
-    models.User.create({ email, password, firstName, lastName }).then(user => res.send(user)).catch(next);
+    const { email, password, firstName, lastName, devType } = req.body;
+    models.User.create({ email, password, firstName, lastName, devType }).then(user => res.send(user)).catch(next);
 })
 
 router.post('/login', (req, res, next) => {
@@ -29,6 +30,11 @@ router.post('/login', (req, res, next) => {
         res.cookie('auth_cookie', token, { httpOnly: true });
         res.send({ user });
     }).catch(next);
+})
+
+router.put('/update', auth, (req, res, next) => {
+    const { id, firstName, lastName, devType } = req.body;
+    models.User.updateOne({ _id: id }, { firstName, lastName, devType }).then(user => res.send(user)).catch(next);
 })
 
 module.exports = router;
