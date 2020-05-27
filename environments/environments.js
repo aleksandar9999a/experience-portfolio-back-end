e64b4c4c85a75ad890c8eb6ff12d74eaa0166426
aleksandar9999a <aleksandar9999a@gmail.com>
2020-05-27 11:47:35 +0300

    Added dbConnectConfig, authCookieName

--- environments/environments.js
+++ environments/environments.js
@@ -2,8 +2,10 @@ const env = process.env.NODE_ENV || 'development';
 const config = {
     development: {
         port: process.env.PORT || 4000,
+        dbConnectConfig: { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true },
         dbURL: 'mongodb://localhost:27017/experience-portfolio',
-        secret: 'qkoedasiumen'
+        secret: 'qkoedasiumen',
+        authCookieName: 'auth_cookie'
     },
     production: {}
 };
