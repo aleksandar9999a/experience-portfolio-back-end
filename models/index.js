a34a7469587aa184b9f599c0e7433d85a6e4e7d0
aleksandar9999a <aleksandar9999a@gmail.com>
2020-05-27 11:47:24 +0300

    Added blacklist

--- models/index.js
+++ models/index.js
@@ -2,10 +2,12 @@ const User = require('./user');
 const AboutMe = require('./aboutMe');
 const Skills = require('./skills');
 const Projects = require('./projects');
+const Blacklist = require('./blacklist');
 
 module.exports = {
   User,
   AboutMe,
   Skills,
-  Projects
+  Projects,
+  Blacklist
 };
