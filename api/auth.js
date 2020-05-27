3ef3f1690e1f1a66e8b354bca96715880ad4c789
aleksandar9999a <aleksandar9999a@gmail.com>
2020-05-27 18:06:30 +0300

    Remove sendUser, added new route '/main' return owner info, '/' return info about user

--- api/auth.js
+++ api/auth.js
@@ -4,46 +4,53 @@ const models = require('./../models/');
 const modules = require('./../modules');
 const { auth } = require('./../modules');
 
-function sendUser(res, isLogged) {
-    return models.User.find().then(users => {
-        if (!!users[0]) {
-            let user = users[0];
-            res.send({
-                email: user.email,
-                firstName: user.firstName,
-                lastName: user.lastName,
-                devType: user.devType,
-                _id: user._id,
-                isLogged
-            });
-            return;
-        }
-        res.sendStatus(404);
-        return;
-    })
-}
-
 router.get('/', (req, res, next) => {
     const token = req.cookies[config.authCookieName];
-    
+
     Promise.all([modules.jwt.verifyToken(token), models.Blacklist.findOne({ token })])
         .then(([data, blacklistToken]) => {
             if (!!blacklistToken) { return Promise.reject(new Error('Token is in blacklist!')) }
-            return sendUser(res, true);
+            return models.User.findById(data.id);
+        })
+        .then(user => {
+            if (user) {
+                res.send(user);
+                return;
+            }
+            res.send(null);
         })
         .catch(err => {
-            const errors = ['jwt must be provided', 'Token is in blacklist!']
-            if (errors.includes(err.message)) {
-                return sendUser(res, false);
+            if (err.message === 'jwt must be provided') {
+                res.send(null);
+                return;
             }
             next(err);
         });
+});
+
+router.get('/main', (req, res, next) => {
+    models.User.find()
+        .then(users => {
+            if (!!users[0]) {
+                let user = users[0];
+                res.send({
+                    firstName: user.firstName,
+                    lastName: user.lastName,
+                    devType: user.devType
+                });
+                return;
+            }
+            res.sendStatus(404);
+            return;
+        })
+        .catch(next);
 });
 
 router.get('/logout', (req, res, next) => {
     const token = req.cookies[config.authCookieName];
     models.Blacklist.create({ token }).then(() => {
         res.clearCookie(config.authCookieName);
+        res.send(null);
     }).catch(next);
 });
 
