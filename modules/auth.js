6db3997b2c9eaab86c6dd62b301f2df2e70e283b
aleksandar9999a <aleksandar9999a@gmail.com>
2020-05-27 11:48:44 +0300

    Refactoring, added blacklist check

--- modules/auth.js
+++ modules/auth.js
@@ -1,18 +1,18 @@
 const jwt = require('./jwt');
 const models = require('../models');
+const config = require('./../environments/environments');
 
 module.exports = function (req, res, next) {
-    const token = req.cookies['auth_cookie'];
-    jwt.verify(token)
-        .then(({ id }) => models.User.findById(id))
+    const token = req.cookies[config.authCookieName] || '';
+    Promise.all([jwt.verifyToken(token), models.Blacklist.findOne({ token })])
+        .then(([data, blacklistToken]) => {
+            if (!!blacklistToken) { return Promise.reject(new Error('Token is in blacklist!')) }
+            return models.User.findById(data.id);
+        })
         .then(user => {
-            if (!user) {
-                return Promise.reject();
-            }
+            if (!user) { return Promise.reject(new Error('User is unknown!')) }
             req.user = user;
             next();
         })
-        .catch(() => {
-            res.status(401).send('[UNAUTHORIZED]');
-        });
+        .catch(next);
 }
