f4b43d85683131356cc738a52f54b5aac1fe1fef
aleksandar9999a <aleksandar9999a@gmail.com>
2020-05-30 21:17:20 +0300

    Make all routes private

--- api/pages.js
+++ api/pages.js
@@ -1,27 +1,29 @@
 const router = require('express').Router();
-const models = require('./../models/');
-const config = require('./../environments/environments');
+const models = require('./../models');
 const { auth } = require('./../modules');
 
-router.get('/about', (req, res, next) => {
-    models.AboutMe.findById(config.defaultAbout)
+router.get('/about', auth, (req, res, next) => {
+    const user = req.user;
+    models.AboutMe.findOne({ creatorId: user._id })
         .then(info => {
             if (!!info) {
                 res.send(info);
                 return;
             }
-            res.send(404);
-        }).catch(next);
+            res.sendStatus(404);
+        })
+        .catch(next);
 });
 
-router.get('/skills', (req, res, next) => {
-    models.Skills.findById(config.defaultSkills)
+router.get('/skills', auth, (req, res, next) => {
+    const user = req.user;
+    models.Skills.findOne({ creatorId: user._id })
         .then(info => {
             if (!!info) {
                 res.send(info);
                 return;
             }
-            res.send(404);
+            res.sendStatus(404);
         }).catch(next);
 });
 
@@ -31,32 +33,56 @@ router.get('/projects', (req, res, next) => {
 
 router.post('/about', auth, (req, res, next) => {
     const { description, courses } = req.body;
-    models.AboutMe.create({ description, courses }).then(info => res.send(info)).catch(next);
+    const user = req.user;
+    models.AboutMe.create({ description, courses, creatorId: user._id })
+        .then(info => res.send(info))
+        .catch(next);
 })
 
 router.post('/Skills', auth, (req, res, next) => {
     const { description, experience } = req.body;
-    models.Skills.create({ description, experience }).then(info => res.send(info)).catch(next);
+    const user = req.user;
+    models.Skills.create({ description, experience, creatorId: user._id })
+        .then(info => res.send(info))
+        .catch(next);
 })
 
 router.post('/projects', auth, (req, res, next) => {
     const { title, description, images } = req.body;
-    models.Projects.create({ title, description, images }).then(project => res.send(project)).catch(next);
+    const user = req.user;
+    models.Projects.create({ title, description, images, creatorId: user.id })
+        .then(project => res.send(project))
+        .catch(next);
 })
 
 router.put('/about', auth, (req, res, next) => {
-    const { id, description, courses } = req.body;
-    models.AboutMe.updateOne({ _id: id }, { description, courses }).then(info => res.send(info)).catch(next);
+    const { id, description, courses, creatorId } = req.body;
+    const user = req.user;
+    if (user._id.toString() === creatorId) {
+        models.AboutMe.updateOne({ _id: id }, { description, courses, creatorId }).then(info => res.send(info)).catch(next);
+    } else {
+        next('Unauthorized');
+    }
 });
 
-router.put('/Skills', auth, (req, res, next) => {
-    const { id, description, experience } = req.body;
-    models.Skills.updateOne({ _id: id }, { description, experience }).then(info => res.send(info)).catch(next);
+router.put('/skills', auth, (req, res, next) => {
+    const { id, description, experience, creatorId } = req.body;
+    const user = req.user;
+    if (user._id.toString() === creatorId) {
+        models.Skills.updateOne({ _id: id }, { description, experience, creatorId }).then(info => res.send(info)).catch(next);
+    } else {
+        next('Unauthorized');
+    }
 });
 
 router.put('/projects', auth, (req, res, next) => {
-    const { id, title, description, images } = req.body;
-    models.Projects.updateOne({ _id: id }, { title, description, images }).then(project => res.send(project)).catch(next);
+    const { id, title, description, images, creatorId } = req.body;
+    const user = req.user;
+    if (user._id.toString() === creatorId) {
+        models.Projects.updateOne({ _id: id }, { title, description, images, creatorId }).then(project => res.send(project)).catch(next);
+    } else {
+        next('Unauthorized');
+    }
 });
 
 module.exports = router;
